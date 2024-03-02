fdb05ff379e651e6228ca9ce78c373037800092b
Chetna0524 <chetnagupta2021@gmail.com>
2024-03-02 16:20:22 +0530

    ssr

---
+++ server/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { StaticRouter } from "react-router-dom/server";
+import ReactDOMServer from "react-dom/server";
+
+import { Provider } from "react-redux";
+
+import App from "../src/App";
+import fs from "fs";
+
+import store from "../src/app/store";
+
+const app = express();
+app.use("/static", express.static(__dirname));
+const PORT = process.env.PORT;
+
+/**
+ * Produces the initial non-interactive HTML output of React
+ * components. The hydrateRoot method is called on the client
+ * to make this HTML interactive.
+ * @param {string} location
+ * @return {Promise<string>}
+ */
+
+const createReactApp = async (location) => {
+	/* await store.dispatch(setCredentials({ user: null, token: null })); */
+
+	const preloadedState = store.getState();
+	const reactApp = ReactDOMServer.renderToString(
+		<Provider store={store}>
+			<StaticRouter location={location} context={{}}>
+				<App />
+			</StaticRouter>
+		</Provider>
+	);
+	const html = await fs.promises.readFile(`${__dirname}/index.html`, "utf-8");
+	const reactHtml = html.replace(
+		'<div id="root"></div>',
+		`<div id="root">${reactApp}</div>
+		<script>
+		window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+			/</g,
+			"\u003c"
+		)}
+	  </script>
+	 
+		`
+	);
+	return reactHtml;
+};
+
+app.get("*", async (req, res) => {
+	const indexHtml = await createReactApp(req.url);
+	res.status(200).send(indexHtml);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
---
+++ webpack.config.cjs
@@ -0,0 +1,83 @@
+const path = require("path");
+const webpack = require("webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+/**
+ * Load JS and JSX files through Babel
+ */
+const babelLoader = {
+	rules: [
+		{
+			test: /.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: [
+						"@babel/preset-env",
+						["@babel/preset-react", { runtime: "automatic" }],
+					],
+				},
+			},
+		},
+		{
+			test: /\.(css|sass|scss)$/,
+			use: [
+				{
+					loader: "style-loader",
+				},
+				{
+					loader: "css-loader",
+				},
+				{
+					loader: "sass-loader",
+				},
+			],
+		},
+	],
+};
+
+const resolve = {
+	extensions: [".js", ".jsx"],
+};
+
+const serverConfig = {
+	target: "node",
+	mode: "development",
+	entry: "./server/server.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "server.cjs",
+	},
+	module: babelLoader,
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			PORT: 3001,
+		}),
+	],
+	resolve,
+};
+
+const clientConfig = {
+	target: "web",
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		/*
+		 * Appends /static to index.html when looking for client.js
+		 * This is where Express is serving static files from
+		 */
+		publicPath: "/static",
+		filename: "client.js",
+	},
+	module: babelLoader,
+	plugins: [
+		new htmlWebpackPlugin({
+			template: `${__dirname}/public/index.html`,
+		}),
+	],
+	resolve,
+};
+
+module.exports = [serverConfig, clientConfig];
