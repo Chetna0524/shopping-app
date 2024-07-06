a6b56aafd75fd3a548bb56b7a1a1c5026d755854
Chetna0524 <159872600+Chetna0524@users.noreply.github.com>
2024-07-06 13:35:22 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCHK8i_DPrEQUa9d8c8C9na39Z-4EbAtQs",
-	authDomain: "shopping-app-18033.firebaseapp.com",
-	projectId: "shopping-app-18033",
-	storageBucket: "shopping-app-18033.appspot.com",
-	messagingSenderId: "1042612953262",
-	appId: "1:1042612953262:web:179ce4135782e92f814d3f",
+	apiKey: "XXX,
+	authDomain: "shopping-app-XXX.firebaseapp.com",
+	projectId: "shopping-app-XXX",
+	storageBucket: "shopping-app-XXX.appspot.com",
+	messagingSenderId: "10426129XXX",
+	appId: "1:1042612953262:web:179ce4135782eXXXX",
 };
 
 // Initialize Firebase
@@ -17,24 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
-/* 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDHTZvqPA8F4bg19J8A7Wra7iOwclMhxgI",
-  authDomain: "shopping-app-d95eb.firebaseapp.com",
-  projectId: "shopping-app-d95eb",
-  storageBucket: "shopping-app-d95eb.appspot.com",
-  messagingSenderId: "954008185536",
-  appId: "1:954008185536:web:1a7a12aa54b7d02aa39b25",
-  measurementId: "G-ZZ61YZBQ4F",
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export const auth = getAuth(app);
- */
